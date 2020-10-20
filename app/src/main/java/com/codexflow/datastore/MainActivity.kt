d8831b6b34391afd41d71e5daeb540a40e38b5ef
inancyillmaz <inancyillmaz@gmail.com>
2020-10-20 23:51:33 +0300

    Initial commit
    Signed-off-by: inancyillmaz <inancyillmaz@gmail.com>

--- app/src/main/java/com/codexflow/datastore/MainActivity.kt
+++ app/src/main/java/com/codexflow/datastore/MainActivity.kt
@@ -4,19 +4,26 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var userManager: UserModel
+    //  lateinit var userManager: StoreLayer
+
+
+    private lateinit var viewModel: StoreViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userManager = UserModel(this)
+
+        viewModel = ViewModelProviders.of(this).get(StoreViewModel::class.java)
+
+
+        // userManager = StoreLayer(this)
 
         var textview = findViewById<TextView>(R.id.textView)
         var textview2 = findViewById<TextView>(R.id.textView2)
@@ -24,30 +31,32 @@ class MainActivity : AppCompatActivity() {
         var ageedittext = findViewById<TextView>(R.id.editText2)
         var buton = findViewById<TextView>(R.id.buton)
 
+        viewModel.Name.observe(this, {
+            textview.text = it
+        })
+
+        viewModel.Age.observe(this, {
+            textview2.text = it.toString()
+        })
+
+
 
         buton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
 
                 if (nameedittext.text.length > 0) {
                     GlobalScope.launch {
-                        userManager.storeUserInfoName(nameedittext.text.toString())
+                        viewModel.saveText(nameedittext.text.toString())
                     }
                 }
                 if (ageedittext.text.length > 0) {
                     GlobalScope.launch {
-                        userManager.storeUserInfoAge(ageedittext.text.toString().toInt())
+                        viewModel.saveNumber(ageedittext.text.toString().toInt())
                     }
                 }
             }
         })
 
 
-        userManager.userNameFlow.asLiveData().observe(this, {
-            textview.text = it
-        })
-        userManager.userAgeFlow.asLiveData().observe(this, {
-            textview2.text = it.toString()
-
-        })
     }
 }
