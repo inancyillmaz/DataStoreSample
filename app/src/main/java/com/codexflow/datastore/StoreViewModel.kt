2f2603869686c2c50591e9ed4e306541434bba0d
inancyillmaz <inancyillmaz@gmail.com>
2020-10-21 14:56:53 +0300

    Initial commit
    Signed-off-by: inancyillmaz <inancyillmaz@gmail.com>

---
+++ app/src/main/java/com/codexflow/datastore/StoreViewModel.kt
@@ -0,0 +1,31 @@
+package com.codexflow.datastore
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class StoreViewModel(
+    application: Application,
+) : AndroidViewModel(application) {
+
+    private val repository = StoreLayer(getApplication())
+
+    val Name = repository.userNameFlow.asLiveData()
+    val Age = repository.userAgeFlow.asLiveData()
+
+    fun saveText(newValue: String)  {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.storeUserInfoName(newValue)
+        }
+    }
+
+    fun saveNumber(newValue: Int)  {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.storeUserInfoAge(newValue)
+        }
+    }
+}
